0a0559b47314d41d4611568ce358883b3e2fc95a
Zhidafeng <635103237@qq.com>
2018-11-02 14:24:25 +0800

    机器人新增和删除

--- admin/routers/router.go
+++ admin/routers/router.go
@@ -6,9 +6,12 @@ import (
 )
 
 func init() {
-    beego.Router("/zgorder/finished", &controllers.MainController{})
+    beego.Router("/zgorder/finished", &controllers.ZGFinishedController{},"*:ZGFinished")
     beego.Router("/zgorder/pending", &controllers.MainController{})
-	beego.Router("/huobiorder/finished", &controllers.MainController{})
+	beego.Router("/huobiorder/finished", &controllers.HuobiFinishedController{},"*:HuobiFinished")
 	beego.Router("/robot/start", &controllers.RobotStartController{},"*:Start")
 	beego.Router("/robot/stop", &controllers.RobotStopController{},"*:Stop")
+	beego.Router("/robot/add", &controllers.RobotManagerController{},"*:Add")
+	beego.Router("/robot/delete", &controllers.RobotManagerController{},"*:Delete")
+	beego.Router("/robot/list", &controllers.RobotManagerController{},"*:RobotsList")
 }
