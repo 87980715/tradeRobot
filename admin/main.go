01b8468c28346dd822467468de8d6a4819d57e5b
Zhidafeng <635103237@qq.com>
2018-11-15 19:45:33 +0800

    runtime

--- admin/main.go
+++ admin/main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/astaxie/beego"
 	"tradeRobot/robot/initialize"
 	"github.com/astaxie/beego/logs"
+	"runtime"
 )
 
 
 
 func main() {
 
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	err := initialize.InitRobot()
 	if err != nil {
 		logs.Error("init robot failed err:",err)
